1a64c665131cb2fe42737f421a8221d2014da4e4
Javier Gonzalez Llamas <lastoner4@gmail.com>
2021-01-21 09:54:26 +0000

    Update sql structure

--- models/blog.js
+++ models/blog.js
@@ -1,16 +1,22 @@
 const db = require('./db.js')
 
+const selectquery = `SELECT bc.title, bc.description, bc.content, l.name language, l.symbol lang_code, c.name category, sc.name subcategory
+                     FROM blogs b
+                     LEFT JOIN blogs_content bc on b.blog_id = bc.blog_id
+                     LEFT JOIN languages l on bc.lang_id = l.lang_id
+                     LEFT JOIN categories c ON b.cat_id = c.cat_id
+                     LEFT JOIN subcategories sc ON b.subcat_id = sc.subcat_id `;
+
 class Blog {
+
     static async getBlogs(req, res) {
-        console.log('Get blogs');
-
-        const sql = 'SELECT * FROM blogs';
-
-        res.send(await db.queryPlain(sql));
+        res.send(await db.queryPlain(selectquery));
     };
 
     static async getLatestBlogs(req, res) {
-      const sql = 'SELECT * FROM BLOGS ORDER BY upload_date DESC LIMIT 5';
+      const sql = selectquery +
+                  `ORDER BY upload_date
+                   DESC LIMIT 5`;
 
       res.send(await db.queryPlain(sql));
     }
@@ -18,9 +24,9 @@ class Blog {
     static async getBlog(req, res) {
         const blogname = req.params.name;
 
-        const where = 'title LIKE ?';
+        const where = 'WHERE bc.title LIKE ?';
         const values = [ '%'+blogname+'%' ];
-        const sql = 'SELECT * FROM blogs WHERE ' + where;
+        const sql = selectquery + where;
 
         res.send(await db.queryWhere(sql, values));
     };
